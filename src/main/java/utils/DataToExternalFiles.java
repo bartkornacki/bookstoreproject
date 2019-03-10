d5727482a6dcbecce6382c8f80196914b1c82c76
Bartek <bartek.kornacki@gmail.com>
2019-03-10 10:46:07 +0100

    Cleaned up

--- src/main/java/utils/DataToExternalFiles.java
+++ src/main/java/utils/DataToExternalFiles.java
@@ -20,18 +20,16 @@ public class DataToExternalFiles {
                     .stream()
                     .map(x -> x.getId() + ";" + x.getName() + ";" + x.getAge())
                     .collect(Collectors.joining("\n"));
-            writeObject(authorFileName, str);
+
+            File file = new File(authorFileName);
+            PrintWriter printWriter = new PrintWriter(file);
+            printWriter.println(str);
+            printWriter.flush();
+            printWriter.close();
+
             System.out.println("The file has been updated.");
         } catch (FileNotFoundException e) {
             System.out.println("The authors file wasn't found. Please ensure the file exists.");
         }
-    }
-
-    public static void writeObject(String fileName, String text) throws FileNotFoundException {
-        File file = new File(fileName);
-        PrintWriter printWriter = new PrintWriter(file);
-        printWriter.println(text);
-        printWriter.flush();
-        printWriter.close();
     }
 }
