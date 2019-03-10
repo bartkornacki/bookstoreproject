e63db86e8b72050f964f3f5e35e4d8a64989c402
Bartek <bartek.kornacki@gmail.com>
2019-03-10 09:45:29 +0100

    Options 1-12 (without test) are up and running

---
+++ src/test/java/utils/DataToExternalFilesTest.java
@@ -0,0 +1,31 @@
+package utils;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DataToExternalFilesTest {
+
+    @BeforeEach
+    void setUp() {
+        String testString = "1;Robert C. Martin;32\n" +
+                "2;Martin Fowler;50\n" +
+                "3;Kent Beck;54\n" +
+                "4;Joshua Bloch;34\n" +
+                "5;Eric Freeman;41\n" +
+                "6;Bert Bates;20\n" +
+                "7;Kathy Sierra;67\n" +
+                "8;Elisabeth Robson;42\n" +
+                "9;asdasddas;432\n" +
+                "10;asdasdasddas;432";
+    }
+
+    @Test
+    void writeAuthorsToFile() {
+    }
+
+    @Test
+    void writeObject() {
+    }
+}
